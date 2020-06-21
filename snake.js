a034adbb78b6c05d156be1b029abde25dd506657
Chandler Lucius <clucius@chandlerlucius.com>
2020-06-21 17:40:44 -0500

    Setup basic starting point for snake moving on start and drawing egg

--- snake.js
+++ snake.js
@@ -1,39 +1,78 @@
-const setupGame = function () {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    //Define egg details
+const snake = function() {
+    //Declare variables
     const eggSize = 20;
-    let eggX = random(width - eggSize);
-    let eggY = random(height - eggSize);
-    let eggCount = 0;
+    const speed = 100;
 
+    let eggX;
+    let eggY;
+    let snakeX;
+    let snakeY;
+    let ctx;
+    
     //Define snake details
-    let snakeHeight = 20;
-    let snakeWidth = 60;
+    const snake = [];
+    let snakeSize = 20;
 
-    const halfWidth = Math.floor(width / 2);
-    const halfHeight = Math.floor(height / 2);
+    const setupGame = function () {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        
+        eggX = random(width - eggSize);
+        eggY = random(height - eggSize);
+    
+        snakeX = Math.floor(width / 2);
+        snakeY = Math.floor(height / 2);
+    
+        const canvas = document.getElementsByClassName("canvas")[0];
+        canvas.width = width;
+        canvas.height = height;
+    
+        ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#FFFFFF";
 
-    const canvas = document.getElementsByClassName("canvas")[0];
-    canvas.width = width;
-    canvas.height = height;
+        snake.push([snakeX, snakeY]);
+        snakeX += snakeSize;
+        snake.push([snakeX, snakeY]);
+        snakeX += snakeSize;
+        snake.push([snakeX, snakeY]);
 
-    const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(halfWidth, halfHeight, snakeWidth, snakeHeight);
-    ctx.fillRect(eggX, eggY, eggSize, eggSize);
+        for(let i = 1; i <= snake.length; i++) {
+            const xy = snake[i - 1];
+            ctx.fillRect(xy[0], xy[1], snakeSize, snakeSize);
+        }
+        ctx.fillRect(eggX, eggY, eggSize, eggSize);
+        
+        moveRight();
+    }
 
-    let movement = 0;
-    setInterval(function () {
-        movement += eggSize;
-        ctx.fillRect(halfWidth + movement, halfHeight, snakeWidth, snakeHeight);
-        ctx.clearRect(halfWidth + movement - snakeWidth, halfHeight, snakeWidth, snakeHeight);
-    }, 100);
+    const moveRight = function() {
+        setInterval(function () {
+            snakeX += snakeSize;
+            ctx.fillRect(snakeX, snakeY, snakeSize, snakeSize);
+            snake.push([snakeX, snakeY]);
+            const old = snake.shift();
+            ctx.clearRect(old[0] - 1, old[1], snakeSize, snakeSize);
+        }, speed);
+    }
+    
+    const handleMove = function (event) {
+        if (event.keyCode == '38') {
+            // up arrow
+        } else if (event.keyCode == '40') {
+            // down arrow
+        } else if (event.keyCode == '37') {
+            // left arrow
+        } else if (event.keyCode == '39') {
+            // right arrow
+        }
+    }
+    
+    const random = function (max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    document.addEventListener('DOMContentLoaded', setupGame);
+    document.addEventListener("keydown", handleMove);
 }
 
-const random = function (max) {
-    return Math.floor(Math.random() * Math.floor(max));
-}
-
-document.addEventListener('DOMContentLoaded', setupGame);
+snake();
